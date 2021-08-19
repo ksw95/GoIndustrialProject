c33d19371fe37748a7b83944b05a7a5e6845d063
ksw95 <kshaowei95@gmail.com>
2021-08-19 15:58:38 +0800

    adding controller files

---
+++ API/controllers/shop_food_controller.go
@@ -0,0 +1,21 @@
+package controllers
+
+import "fmt"
+
+//Retrieve All Restaurants
+func GetRestaurants() {
+	fmt.Println("This is to get restaurants") //delete after changing the function
+
+}
+
+//Return Restaurants based on search
+func SearchRestaurant() {
+	fmt.Println("This is to search for restaurants") //delete after changing the function
+
+}
+
+//Retrieve Food from restaurant
+func GetFood() {
+	fmt.Println("This is to get food information for a restaurant") //delete after changing the function
+
+}
---
+++ API/controllers/user_controller.go
@@ -0,0 +1,27 @@
+package controllers
+
+import "fmt"
+
+//New User Account Creation
+func Register() {
+	//Receive parameters from client
+	//Check username whether exist in database
+	//Create new entry in database
+	fmt.Println("This is for registering") //delete after changing the function
+}
+
+//User Login
+func Login() {
+	//Receive params from client
+	//Check username and password
+	//Assign JWT with UserCond info
+	fmt.Println("This is for login") //delete after changing the function
+}
+
+//Updating User Conditions
+func UserCondition() {
+	//Receive params from client
+	//Check Account via JWT
+	//Update into database
+	fmt.Println("This is for user condition") //delete after changing the function
+}
