69108cfd4a0d1675867a48f6c7816cdc1c687c7c
Shinokyofu <kaizer0001@gmail.com>
2021-08-23 07:46:25 +0800

    Create history.go

---
+++ API/models/history.go
@@ -0,0 +1,11 @@
+package models
+
+type History struct {
+	ID	            int
+	Username	      string
+  FoodPurchase	  string
+  Price	          float
+  DeliveryMode	  string
+  Distance        float
+  CaloriesBurned	int
+}
