1fcaf2c0ee1e5ac9be6459475a581354251ba5ed
ksw95 <kshaowei95@gmail.com>
2021-08-29 18:53:25 +0800

    Edited Register function in user controller

--- API/controller/user_controller.go
+++ API/controller/user_controller.go
@@ -1,20 +1,40 @@
 package controller
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/ksw95/GoIndustrialProject/API/models"
+	"github.com/labstack/echo"
+	"golang.org/x/crypto/bcrypt"
+)
 
 //New User Account Creation
-func Register() {
+func Register(c echo.Context) {
 	//Receive parameters from client
+	user := models.Account{}
+	err := json.NewDecoder(c.Request().Body).Decode(&user)
+	if err != nil {
+		return newResponse(c, "Bad Request", "false", http.StatusBadRequest, nil)
+
+	}
+	user.Password, err = bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.MinCost)
+	if err != nil {
+		return newResponse(c, "Internal Error", "false", http.StatusInternalServerError, nil)
+	}
 	//Check username whether exist in database
 	//Create new entry in database
-	fmt.Println("This is for registering") //delete after changing the function
+	
 }
 
 //User Login
-func Login() {
+func Login(c echo.Context) {
 	//Receive params from client
 	//Check username and password
 	//Assign JWT with UserCond info
+	user := models.Account{}
+	err := json.NewDecoder(c.Request().Body).Decode(&user)
 	fmt.Println("This is for login") //delete after changing the function
 }
 
--- API/go.mod
+++ API/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/go-sql-driver/mysql v1.6.0
+	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.3.0
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
