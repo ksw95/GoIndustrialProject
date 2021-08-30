a88c509af265601cc8297c8b1f70dcdb9c0e38ae
ksw95 <kshaowei95@gmail.com>
2021-08-30 17:44:38 +0800

    Edited Register function in user controller

--- API/controller/user_controller.go
+++ API/controller/user_controller.go
@@ -11,20 +11,29 @@ import (
 )
 
 //New User Account Creation
-func Register(c echo.Context) {
+func (dbHandler *DBHandler) Register(c echo.Context) {
 	//Receive parameters from client
 	user := models.Account{}
 	err := json.NewDecoder(c.Request().Body).Decode(&user)
 	if err != nil {
-		return newResponse(c, "Bad Request", "false", http.StatusBadRequest, nil)
+		newResponse(c, "Bad Request", "false", http.StatusBadRequest, nil)
+		return
 	}
-	user.Password, err = bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.MinCost)
+	hashPW, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.MinCost)
 	if err != nil {
-		return newResponse(c, "Internal Error", "false", http.StatusInternalServerError, nil)
+		newResponse(c, "Internal Error", "false", http.StatusInternalServerError, nil)
+		return
 	}
+	user.Password = string(hashPW)
 	//Check username whether exist in database
 	//Create new entry in database
-	
+	_, err = dbHandler.DB.Exec("INSERT INTO my_db.Account VALUES (?,?)", user.Username, user.Password)
+	if err != nil {
+		newResponse(c, "Failed to create new account", "false", http.StatusForbidden, nil)
+		return
+	}
+	newResponse(c, "Sucessfully created", "true", http.StatusCreated, nil)
+	return
 }
 
 //User Login
